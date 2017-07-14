a78f7b363f087e4a1e63e19b95dec982346bf0c0
Marcio <marcio@mail.com>
2017-07-13 23:19:47 -0300

    change format validation link

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,8 @@ include PgSearch
 
 class Link < ActiveRecord::Base
   validates_presence_of :url, :company_id
-  validates_format_of :url, :with => /\A(http|https|www):(\/\/)|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, :if => Proc.new{|l| l.url.present?}
+  validates :url, :format => {:with => URI.regexp}, :if => Proc.new{|l| l.url.present?}
+  # validates_format_of :url, :with => /\A(http|https|www):(\/\/)|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, :if => Proc.new{|l| l.url.present?}
   validates_uniqueness_of :url
 
   has_many :link_hashtags
@@ -12,12 +13,13 @@ class Link < ActiveRecord::Base
   belongs_to :company
 
   pg_search_scope :search, :against => :url
-  before_save :add_protocol
+
+  before_validation :add_protocol, :on => :create
   
   private 
   
   def add_protocol
-    unless self.url =~ /\Ahttp:\/\//
+    unless self.url =~ /\Ahttp:\/\// || self.url =~ /\Ahttps:\/\//
       self.url = "http://#{self.url}"
     end
   end
--- spec/services/link/create_service_spec.rb
+++ spec/services/link/create_service_spec.rb
@@ -3,16 +3,21 @@ require_relative './../../spec_helper.rb'
 describe LinkModule::CreateService do
   before do
     @company = create(:company)
-    @link = create(:link, company: @company)
+    @link = build(:link, company: @company)
     @hashtags = "#{FFaker::Lorem.word}, #{FFaker::Lorem.word}"
     @params_valid = {"url-original" => @link.url, "hashtags-original" => @hashtags}
-    @params_invalid = {"url-original" => @link.url}    
+    @params_invalid = {"url-original" => "ttt", "hashtags-original" => @hashtags}    
   end
   describe "#call" do
-    it "with hashtags params invalid, return an error" do 
+    it "with url params invalid, return an error url invalid" do 
       response = LinkModule::CreateService.new(@params_invalid).call
-      expect(response).to match("Hashtag Obrigatória")
+      expect(response).to match("*Url is invalid\n")
     end
+    it "with url params invalid, return an error url invalid" do
+      link = create(:link, company: @company)
+      response = LinkModule::CreateService.new({"url-original" => link.url, "hashtags-original" => @hashtags}).call
+      expect(response).to match("*Url has already been taken\n")
+    end    
     it "with hashtags params valid, return an error" do 
       response = LinkModule::CreateService.new(@params_valid).call
       expect(response).to match("Criado com sucesso")
