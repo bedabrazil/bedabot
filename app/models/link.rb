b4f156c3fb356b919d0c63da7b3d94d3506d8f2a
Marcio <marcio@mail.com>
2017-07-13 22:47:27 -0300

    change how add link

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ include PgSearch
 class Link < ActiveRecord::Base
   validates_presence_of :url, :company_id
   validates_uniqueness_of :url
-  validates :url, :format => {:with => URI.regexp}, :if => Proc.new{|l| l.url.present?}
+  validates_format_of :url, :with => /\A(http|https|www):(\/\/)|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, :if => Proc.new{|l| l.url.present?}
 
   has_many :link_hashtags
   has_many :hashtags, :through => :link_hashtags  
@@ -12,4 +12,13 @@ class Link < ActiveRecord::Base
   belongs_to :company
 
   pg_search_scope :search, :against => :url
+  before_save :add_protocol
+  
+  private 
+  
+  def add_protocol
+    unless self.url[/\Ahttp:\/\//]
+      self.url = "http://#{self.url}"
+    end
+  end
 end
