4cf1af227526663c1d2b9321e501a074a9fd4fc0
Marcio <marcio@mail.com>
2017-07-14 22:22:27 -0300

    change list help

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -8,6 +8,5 @@ class Faq < ActiveRecord::Base
   has_many :hashtags, through: :faq_hashtags
   belongs_to :company
 
-  # include PgSearch
   pg_search_scope :search, :against => [:question, :answer]
 end
--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -1,18 +1,30 @@
 
 class HelpService
   def self.call
-    response  = "*O que eu sei fazer :raised_hands:* \n\n"
-    response += "*help*\n"
-    response += ">Lista de comandos que eu conheço\n\n"
-    response += "*Adicione ao Faq*\n"
-    response += ">Adiciona uma nova questão ao Faq\n\n"
-    response += "*Remova ID*\n"
-    response += ">Remove uma questão baseada no ID dela\n\n"
-    response += "*O que você sabe sobre X*\n"
-    response += ">Pesquisa por palavra na lista de perguntas e respostas\n\n"
-    response += "*Pesquise a hashtag X*\n"
-    response += ">Lista as perguntas e respostas com aquela hashtag\n\n"
-    response += "*Perguntas e Respostas*\n"
-    response += ">Mostra a lista de perguntas e respostas\n\n"
+    response  = "*O que eu sei fazer :raised_hands:*\n\n"
+    response << "*Mostrar estes comandos*\n"
+    response << ">help\n\n>Ajuda\n\n"
+
+    response << "*Adiciona uma nova questão ao Faq*\n"
+    response << ">Adicione ao Faq\n\n>Adicionar Faq\n\n>Adicione uma Questão\n\n"
+
+    response << "*Adiciona um novo Link*\n"
+    response << ">Adicionar ao link\n\n>Adicionar link\n\n>Adicione um link\n\n"
+
+    response << "*Remove uma questão ou Link baseado no ID/Número*\n"
+    response << ">Remover faq ID\n\n>Deletar a questão ID\n\n>Deletar link ID\n\n>Remover Link ID\n\n"
+
+    response << "*Pesquisar por tags na lista de perguntas e respostas*\n\n"
+    response << ">O que você conhece sobre X\n\n"
+    response << ">Encontre a hashtag X\n\n"
+    response << ">Pesquisar sobre X\n\n"
+    response << ">Pesquise a hashtag X\n\n"
+    response << ">Hashtag X\n\n"
+    
+    response << "*Pesquisar por tags na lista de Links*\n\n"
+    response << ">O que você sabe sobre X\n\n"
+    response << ">Buscar a tag X\n\n"
+    response << ">Buscar a hashtag X\n\n"
+        
   end
 end
